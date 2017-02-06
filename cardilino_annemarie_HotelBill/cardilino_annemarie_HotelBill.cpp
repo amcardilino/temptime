b7b188a7a75ad3ee3ce699c49c25e853f0cba346
Anne Marie Cardilino <amcardilino@gmail.com>
2017-02-06 00:43:59 -0500

    staging tip homework

--- cardilino_annemarie_HotelBill/cardilino_annemarie_HotelBill.cpp
+++ cardilino_annemarie_HotelBill/cardilino_annemarie_HotelBill.cpp
@@ -9,35 +9,33 @@ using namespace std;
 
 int main()
 {
-int nu_nights = 6
+int nu_nights = 6;
 
 double fee = 10.00,
     resort_tax = .085,
     room_rate = 100.00,
-    deposit_rate = .10,
-    room_charge, total_fees, total_charges, deposits, total_tax, complete_bill;
-
-room_charge = nu_nights * room_rate;
-total_fees = fee * nu_nights;
-total_charges = room_charge + total_fees;
-total_tax = resort_tax * total_charges;
-deposits = total_charges * deposit_rate;
-complete_bill = room_charge + total_fees + total_charges + total_tax + deposits;
+    deposit_rate = .10, 
+    room_charge = nu_nights * room_rate,
+    total_fees = fee * nu_nights,
+    total_charges = room_charge + total_fees,
+    total_tax = resort_tax * total_charges,
+    deposits = total_charges * deposit_rate,
+    complete_bill = room_charge + total_fees + total_tax + deposits;
 
 cout<< "Thank you for choosing Imaginary Hotel. Now pest free!"<< endl;
-\n
+cout<< endl;
 cout<< "Length of Stay: " << nu_nights << endl;
 cout<< "Daily Rate: $" << room_rate << endl;
 cout<< "Daily Resort Fee: $" << fee << endl;
 cout<< "Resort Tax Rate: $" << resort_tax << endl;
-\n
+cout<< endl;
 cout<< "Itemized Bill"<< endl;
-\n
+cout<< endl;
 cout<< "Total Room Charge: $" << room_charge << endl;
 cout<< "Total Fees: $" << total_fees << endl;
 cout<< "Total Taxes: $" << total_tax << endl;
-cout<< "Total Deposit: $" << deposit << endl;
-\n
+cout<< "Total Deposit: $" << deposits << endl;
+cout<< endl;
 cout<< "Amount due at check in: $" << complete_bill << endl;
 
 
