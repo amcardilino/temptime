0491d611c57601bebc7498b7bba32e48fc7096aa
Anne Marie Cardilino <amcardilino@gmail.com>
2017-02-23 15:15:08 -0500

    committing projects from the past month for cis1111

--- cardilino_annemarie_CIS1111_if.else/cardilino_annemarie_CIS1111_calculatingshippingcharges.cpp
+++ cardilino_annemarie_CIS1111_if.else/cardilino_annemarie_CIS1111_calculatingshippingcharges.cpp
@@ -1,56 +1,134 @@
+//Anne Marie Cardilino
+//02.18.17
+//Calculating Shipping Charges
+//This program is to help calculate
+//shipping items based on weight
+//and distance.
+
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main()
 {
-int weight,
-    distance;
-double rate_of_shipment,
-    total_cost_ship;
+	int weight,
+		distance;
+	double rate_of_shipment,
+		ship_segment;
 
-cout << "Welcome! Please enter your shipping \n";
-cout << "info here so we can take your money!" << endl;
 
+
+	//intro statements for shipping charges
+cout << "Welcome! Please enter your shipping info here so\n";
+cout << "we can take your money!\n" << endl;
+cout << "In case you have not used our services before, here\n";
+cout << "are our current rates per 500 miles:" << endl;
+cout << "\t-5 lbs or less			$1.10" << endl;
+cout << "\t-5 lbs to 15 lbs		$2.20" << endl;
+cout << "\t-16 lbs to 30 lbs		$3.70" << endl;
+cout << "\t-31 lbs to 50 lbs		$4.80" << endl;
+cout << "NOTICE. We are unable to ship packages weighing" << endl;
+cout << "over 50 lbs." << endl;
+cout << "We are also unable to ship packages going less than \n";
+cout << "10 mi or more than 3000 mi.\n" << endl;
+cout << "__________________________________________________________\n" << endl;
 cout << "\nPlease enter the weight of your package: ";
-cin >> weight;
+cin >> weight;         //input weight for rates
 
-if (weight < 6 )
-    {
-        rate_of_shipment = 1.10
-        cout << "Your rate of shipment is $";
-        cout << rate_of_shipment << endl;
-    }
+
+	//if else for weight
+	bool over_weight = false;
+	if (weight >= 50)
+		over_weight = true;
+	else
+		over_weight = false;
+
+if (weight < 6)
+	{
+		rate_of_shipment = 1.10;
+		cout << "Your rate of shipment is $" << setprecision(2) << fixed << rate_of_shipment;
+	}
 else if (weight < 16)
-    {
-        rate_of_shipment = 2.20
-        cout << "Your rate of shipment is $";
-        cout << rate_of_shipment << endl;
-    }
+	{
+		rate_of_shipment = 2.20;
+		cout << "Your rate of shipment is $" << setprecision(2) << fixed << rate_of_shipment;
+	}
 else if (weight < 31)
-    {
-        rate_of_shipment = 3.70
-        cout << "Your rate of shipment is $";
-        cout << rate_of_shipment << endl;
-    }
-else if (weight < 50)
-    {
-        rate_of_shipment = 4.80
-        cout << "Your rate of shipment is $";
-        cout << rate_of_shipment << endl;
-    }
+	{
+		rate_of_shipment = 3.70;
+		cout << "Your rate of shipment is $" << setprecision(2) << fixed << rate_of_shipment;
+	}
+else if (weight < 51)
+	{
+		rate_of_shipment = 4.80;
+		cout << "Your rate of shipment is $" << setprecision(2) << fixed << rate_of_shipment;
+	}
 else
-    {
-        cout << "We cannot ship over 50lbs." << endl;
-    }
+		cout << "We are unable to ship this distance." << endl;
 
-//entering distance of shipping
-    //distance in segments of 500 will determine
-    //how many times the fee will be applied
-cout << "Please enter your shipping distance: ";
+
+
+
+	//entering distance of shipping
+	//distance in segments of 500 will determine
+	//how many times the fee will be applied
+cout << "\nPlease enter your shipping distance: ";
 cin >> distance;
 
 
+	//if else for output for distance
+if (distance < 11 || distance > 3000)
+cout << "ERROR. We cannot ship this distance." << endl;
+else if (distance < 501)
+	{
+		ship_segment = rate_of_shipment;
+		cout << "Your shipment has 1 segment." << endl;
+	}
+else if (distance < 1001)
+	{
+		ship_segment = 2 * rate_of_shipment;
+		cout << "Your shipment has 2 segments." << endl;
 
-    system("pause");
-    return 0;
+	}
+else if (distance < 1501)
+	{
+		ship_segment = 3 * rate_of_shipment;
+		cout << "Your shipment has 3 segments." << endl;
+
+	}
+else if (distance < 2001)
+	{
+		ship_segment = 4 * rate_of_shipment;
+		cout << "Your shipment has 4 segments." << endl;
+
+	}
+else if (distance < 2501)
+	{
+		ship_segment = 5 * rate_of_shipment;
+		cout << "Your shipment has 5 segments." << endl;
+
+	}
+else if (distance < 3001)
+	{
+		cout << "Your shipment has 6 segments." << endl;
+		ship_segment = 6 * rate_of_shipment;
+
+	}
+else
+	cout << "ERROR. We cannot ship this distance." << endl;
+	
+        //final output statements
+cout << "\nThank you for your input. Your total charges" << endl;
+cout << "will be based on the information provided.\n" << endl;
+cout << "__________________________________________________________\n" << endl;
+cout << "This will bring your charge total to $";
+cout << ship_segment << endl;
+cout << "Thank you for shipping with us." << endl;
+cout << "Lowest rates in town! We're so cheap, you'll\n";
+cout << "think we're clumsier than we are!\n" << endl;
+
+
+
+	system("pause");
+	return 0;
 }
--- cardilino_annemarie_tips/cardilino_annemarie_tips_pt2.cpp
+++ cardilino_annemarie_tips/cardilino_annemarie_tips_pt2.cpp
@@ -1,58 +1,34 @@
 #include<iostream>
-#include<string>
-
+//using the modulus means remainder
+//can you use double with modulus
+//or does it have to be integer since the whole point
+//of modulus is to have a remainder?
 using namespace std;
+
 int main()
 {
-int enter_penny,
-    enter_nickel,
-    enter_dime,
-    enter_quarter,
-    enter_dollar;
-//where I'm storing the amount given'
+int change;
 
-double penny = .01,
-    nickel = .05,
-    dime = .10,
-    quarter = .25,
-    dollar = 1.00;
-//where I'm storing the values of the variables'
+cout << "How many pennies do you have?" << endl;
+cin >> change;
+cout <<"Here is a breakdown of change for least coinage: "<<endl;
+//start biggest to smallest
+int nu_dollars = change/100;
+int change1 = change%100;                  //for remainder after dollars
+int nu_quarters = change1/25;
+int change2 = change1%25;                  //for remainder after quarters
+int nu_dimes = change2/10;
+int change3 = change2%10;                  //for remainder after dimes
+int nu_nickels = change3/5;
+int change4 = change3%5;                   //for remainder after nickels
+int nu_pennies = change4/1;                //pennies should end it
+cout << "Dollars: " << nu_dollars << endl;
+cout << "Quarters: " << nu_quarters << endl;
+cout << "Dimes: " << nu_dimes << endl;
+cout << "Nickels: " << nu_nickels << endl;
+cout << "Pennies: " << nu_pennies << endl;
 
-string name;
-//where I'm keeping the server's name
 
-cout << "Hello server. Enter name: " << endl;
-getline(cin, name);
-cout << "Thank you, " << name << endl;
-cout<< "Please enter the tips you've made today: " <<endl;
-cout << "Number of pennies: "<<endl;
-cin >> enter_penny;
-cout<<"Number of nickels: "<<endl;
-cin>> enter_nickel;
-cout<<"Number of dimes: "<<endl;
-cin >> enter_dime;
-cout<<"Number of quarters: "<<endl;
-cin>> enter_quarter;
-cout<<"Number of dollars: "<<endl;
-cin>> enter_dollar;
-
-double total_penny = enter_penny * penny,
-    total_nickel = enter_nickel * nickel,
-    total_dime = enter_dime * dime,
-    total_quarter = enter_quarter * quarter,
-    total_dollar = enter_dollar * dollar,
-    deposit = total_penny + total_nickel + total_dime + total_quarter + total_dollar;
-
-cout << "\nCongrats! You have made more money in one night than the girl who programmed this!"<<endl;
-cout << "\nHere are your totals based on coinage:"<<endl;
-cout << "$" << total_penny << " in pennies."<<endl;
-cout << "$" << total_nickel <<" in nickels."<<endl;
-cout <<"$" << total_dime << " in dimes."<<endl;
-cout << "$" << total_quarter << " in quarters."<<endl;
-cout << "$" << total_dollar << " in dollars."<<endl;
-cout << "\nYour overall total for the night: "<<endl;
-cout << "$" << deposit << endl;
-
-    system ("pause");
-    return 0;
+system ("pause");
+return 0;   
 }
