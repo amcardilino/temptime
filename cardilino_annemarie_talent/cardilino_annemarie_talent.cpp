a271bb72a94b07042302ebcf53a199f1bf8593c5
Anne Marie Cardilino <amcardilino@gmail.com>
2017-04-09 22:56:23 -0400

    committing corrected code but still don’t think that the name input is right

--- cardilino_annemarie_talent/cardilino_annemarie_talent.cpp
+++ cardilino_annemarie_talent/cardilino_annemarie_talent.cpp
@@ -4,37 +4,42 @@
 using namespace std;
 
 //function for average
+//function for highest average score
 //function for lowest
 //function for highest
 //function for validation
 
 double average_rank(double);
-int top_scorer(string, double &, int);
+double lead(double, double);
 int low_score(int, int, int, int, int);
 int high_score(int, int, int, int, int);
 int validation(int);
 
+
+
 int main()
 {
-	string name;
-	int score1,
-		score2,
-		score3,
-		score4,
-		score5,
-		accum_score,
-		rid_high_low,
-		contestant;
-	double hold_sum;
+	string name,			//name of contestant
+		prenom = " - ";		//original name
+	int score1,				//judge 1 score
+		score2,				//judge 2 score
+		score3,				//judge 3 score
+		score4,				//judge 4 score
+		score5,				//judge 5 score
+		accum_score,		//collects all scores		
+		rid_high_low,		//scores with the highest and lowest values removed
+		contestant;			//to continue with more contestants
+	double hold_sum,		//holds sum for function average_rank
+		initial_top = 0.0;	//initializing top scorer in lead function
 
-	cout << "Welcome to Sinclair's got Talent\n";
+	cout << "Welcome to Sinclair's got Talent!\nLet's see if we actually have any.\n";
 	contestant = 1;
-	
 	while (contestant == 1)
 	{
-		cout << "What's this contestant's name?\n";
-		cin>>name;
-		cout << "\nThank you, " << name << ". Let's turn it\nto the judges:\n\n";
+		cout << "_______________________________________________________________________\n";
+		cout << "\nWhat's this contestant's name?\n";
+		cin >> name;											//name input
+		cout << "Let's turn it to the judges:\n\n";
 		cout << "Please enter Judge Chuck Norris' score:\t ";
 		cin >> score1;
 		validation(score1);									//validation function
@@ -50,42 +55,51 @@ int main()
 		cout << "Please enter Judge Judy's score:\t ";
 		cin >> score5;
 		validation(score5);									//validation function
+
+			//accumulate the scores
 		accum_score = score1 + score2 + score3 + score4 + score5;
+	
+			//functions that remove the high and the low scores to find the average:
 		rid_high_low = accum_score - high_score(score1, score2, score3, score4, score5) - low_score(score1, score2, score3, score4, score5);
-		cout << "The average score for " << name << " is "; 
+			//function holds average score
 		hold_sum = average_rank(rid_high_low);
+		
+		cout << "\nThe average score for " << name << " is " << hold_sum << endl;
+		cout << "_______________________________________________________________________\n";
+
+			//hold top score function
+		initial_top = lead(hold_sum, initial_top);
+		if (hold_sum == initial_top)
+			prenom = name;
 
 		cout << "\n\nAre there any more contestants?\nPress 1 for yes, and any other for no: ";
-		cin >> contestant;
-		top_scorer(name, hold_sum, contestant);
-	}
+		cin >> contestant;									//choose whether to loop around
 
+	}
+	cout << "Thanks for the auditions!\n\n";
+	cout << prenom << " takes the cake with a grand score of " << initial_top << endl;
 
 	system("pause");
 	return 0;
 }
 
+//function to average scores
 double average_rank(double collective)
 {
 	collective /= 3;
 	cout << fixed << setprecision(2) << collective << endl;
 	return collective;
 }
-int top_scorer(string name, double &average, int contestant)
+
+//function to remember top score
+double lead(double top, double initial)
 {
-	average;
-	string winning;
-	double win_over = 0.1;
-
-	if (average > win_over)
-	{
-		win_over = average;
-		winning = name;
-	}
-	cout << "Our current leader is " << winning << " with\n\tan average score of " << win_over << endl;
-
-	return win_over;
+	if (top > initial)
+		initial = top;
+	return initial;
 }
+
+//function to remove high score
 int high_score(int score1, int score2, int score3, int score4, int score5)
 {
 	int highest = 0;
@@ -101,6 +115,8 @@ int high_score(int score1, int score2, int score3, int score4, int score5)
 		highest = score5;
 	return highest;
 }
+
+//function to remove low score
 int low_score(int score1, int score2, int score3, int score4, int score5)
 {
 	int lowest = 10;
